53eef2f8219a3026702ce413c2568a327a58a9a4
Ben Fowler <bfowler40@gmail.com>
2018-01-11 08:01:07 +1100

    configure exercise 11

---
+++ 11-custom-video-player/README.md
@@ -0,0 +1,13 @@
+# Exercise 11 - Custom Video Player
+
+Manage html video from user interactions.
+
+## Tooling
+
+Serve:
+
+`npm run exercise-10`
+
+Build:
+
+`npm build exercise-10`
---
+++ 11-custom-video-player/js/video-player.ts
@@ -0,0 +1,11 @@
+
+class VideoPlayer {
+	public init() {
+		console.log('video player');
+	}
+}
+
+const videoPlayer = new VideoPlayer();
+videoPlayer.init();
+
+export default videoPlayer;
