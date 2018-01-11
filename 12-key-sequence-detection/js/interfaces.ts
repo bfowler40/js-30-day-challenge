4e8a29a11de1b6a0ab59af408dafe9bd39719b61
Ben Fowler <bfowler40@gmail.com>
2018-01-12 08:41:30 +1100

    solution for exercise 12 :grin:

---
+++ 12-key-sequence-detection/js/interfaces.ts
@@ -0,0 +1,3 @@
+export interface IPasscode {
+	pressed: string[];
+}
--- 12-key-sequence-detection/js/passcode.ts
+++ 12-key-sequence-detection/js/passcode.ts
@@ -1,7 +1,71 @@
+import { IPasscode } from './interfaces';
 
-class Passcode {
+declare const cornify_add: any;
+
+class Passcode implements IPasscode {
+	public pressed: string[] = [];
+	private passcode: string = 'pretzel';
+
+	/**
+	 * Init the class
+	 */
 	public init() {
-		console.log('passcode');
+		window.addEventListener('keyup', this._onKeyUp.bind(this));
+	}
+
+	/**
+	 * Get the pressed keys
+	 *
+	 * @return {array}
+	 */
+	private get pressedKeys(): string[] {
+		return this.pressed;
+	}
+
+	/**
+	 * Set the pressed keys
+	 */
+	private set pressedKeys(keys: string[]) {
+		this.pressed = keys;
+	}
+
+	/**
+	 * Get the secret code
+	 */
+	private get secret(): string {
+		return this.passcode;
+	}
+
+	/**
+	 * Handle the key up event
+	 *
+	 * @return {void}
+	 */
+	protected _onKeyUp(e: KeyboardEvent): void {
+		const pressed = this.pressedKeys;
+
+		if (e.key !== 'Meta') {
+			pressed.push(e.key);
+			this.pressedKeys = [...pressed];
+		}
+
+		this._testSecret();
+	}
+
+	/**
+	 * Test if the secret code has been keyed
+	 *
+	 * @return {void}
+	 */
+	protected _testSecret(): void {
+		// remove the number of keys from the end of pressed array
+		// equal to the length of the secret passcode
+		this.pressedKeys.splice(-this.secret.length - 1, this.pressedKeys.length - this.secret.length);
+		// create a string and test if it matches the passcode
+		if (this.pressedKeys.join('') === this.secret) {
+			console.log('YOU GOT THE PASSCODE!');
+			cornify_add();
+		}
 	}
 }
 
