8c7d6e3847807bb05edbf4ce80251c31ab50ef19
Ben Fowler <bfowler40@gmail.com>
2018-01-11 20:02:43 +1100

    complete exercise 11

--- 11-custom-video-player/js/video-player.ts
+++ 11-custom-video-player/js/video-player.ts
@@ -24,16 +24,33 @@ class VideoPlayer {
 	 * @return {void}
 	 */
 	public init(): void {
+
+		// Video event listeners
 		this.video.addEventListener('click', this._togglePlay.bind(this));
 		this.video.addEventListener('play', this._updateButton.bind(this));
 		this.video.addEventListener('pause', this._updateButton.bind(this));
 		this.video.addEventListener('timeupdate', this._handleProgress.bind(this));
 
+		// Toggle play pause
 		this.toggle.addEventListener('click', this._togglePlay.bind(this));
 
+		// Skip forward/backward
 		this.skipButtons.forEach((button: HTMLElement) => {
 			button.addEventListener('click', this._skip.bind(this));
 		});
+
+		// Range inputs: volume, playback speed
+		this.ranges.forEach((range: HTMLInputElement) => {
+			range.addEventListener('change', this._handleRangeUpdate.bind(this));
+			range.addEventListener('mousemove', this._handleRangeUpdate.bind(this));
+		});
+
+		// Scrub video
+		let mousedown = false;
+		this.progress.addEventListener('click', this._scrub.bind(this));
+		this.progress.addEventListener('mousemove', (e) => mousedown && this._scrub(e));
+		this.progress.addEventListener('mousedown', () => mousedown = true);
+		this.progress.addEventListener('mouseup', () => mousedown = false);
 	}
 
 	/**
@@ -65,8 +82,8 @@ class VideoPlayer {
 	 */
 	protected _handleProgress(): void {
 		const percent: number = (this.video.currentTime / this.video.duration) * 100;
-		// progressBar.style.flexBasis = `${percent}%`;
-		console.log('video progress: ', percent);
+
+		this.progressBar.style.flexBasis = `${percent}%`;
 	}
 
 	/**
@@ -76,10 +93,31 @@ class VideoPlayer {
 	 */
 	protected _skip(e: Event): void {
 		const button: HTMLButtonElement = (e.target as HTMLButtonElement);
-		console.log('video skip: ', button.dataset.skip);
+
 		this.video.currentTime += parseFloat(button.dataset.skip);
 	}
 
+	/**
+	 * Hangle range update - volume, speed
+	 *
+	 * @return {void}
+	 */
+	protected _handleRangeUpdate(e: Event) {
+		const target = (e.target as HTMLInputElement);
+
+		this.video[target.name] = target.value;
+	}
+
+	/**
+	 * Scrub
+	 *
+	 * @return {void}
+	 */
+	protected _scrub(e: MouseEvent) {
+		const scrubTime: number = (e.offsetX / this.progress.offsetWidth) * this.video.duration;
+
+		this.video.currentTime = scrubTime;
+	}
 }
 
 const videoPlayer = new VideoPlayer();
