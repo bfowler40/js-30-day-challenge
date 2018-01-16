9d2effd7e9fb6eeb5845623efe5d8b1396ca7693
Ben Fowler <bfowler40@gmail.com>
2018-01-17 08:51:26 +1100

    solution to exercise 17

---
+++ 17-sorting-list/js/interfaces.ts
@@ -0,0 +1,4 @@
+
+export interface ICleanSort {
+	bands: string[];
+}
--- 17-sorting-list/js/sorter.ts
+++ 17-sorting-list/js/sorter.ts
@@ -1,3 +1,56 @@
-{
-	console.log('sorter');
+import { ICleanSort } from './interfaces';
+
+class CleanSort {
+	public list: string[];
+
+	constructor(list: string[]) {
+		this.list = list;
+	}
+
+	/**
+	 * Sort the array and list alphabetically
+	 *
+	 * @param {string} selector
+	 * @return {void}
+	 */
+	public listAlpha(selector: string = 'body'): void {
+		// sort list without conjunctions
+		const sortedList = bands.sort(
+			(prev: string, current: string) => this._strip(prev) > this._strip(current) ? 1 : -1
+		);
+		// append to provided selector
+		document.querySelector(selector).innerHTML =
+			sortedList.map((item: string): string => `<li class="list-item">${ item }</li>`).join('');
+	}
+
+	/**
+	 * Remove some words from string
+	 *
+	 * @param {string} item
+	 * @return {string}
+	 */
+	protected _strip(item: string): string {
+		return item.replace(/^(a |the |an )/i, '').trim();
+	}
 }
+
+const bands: string[] = [
+	'The Plot in You',
+	'The Devil Wears Prada',
+	'Pierce the Veil',
+	'Norma Jean',
+	'The Bled',
+	'Say Anything',
+	'The Midway State',
+	'We Came as Romans',
+	'Counterparts',
+	'Oh, Sleeper',
+	'A Skylit Drive',
+	'Anywhere But Here',
+	'An Old Dog',
+];
+
+const cleanSort = new CleanSort(bands);
+cleanSort.listAlpha('#list');
+
+export default cleanSort;
