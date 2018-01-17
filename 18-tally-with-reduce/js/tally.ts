66578d4dc201da89431170fad136a07703dd7ab3
Ben Fowler <bfowler40@gmail.com>
2018-01-18 08:59:19 +1100

    add solution to exercise 18

--- 18-tally-with-reduce/js/tally.ts
+++ 18-tally-with-reduce/js/tally.ts
@@ -1,3 +1,25 @@
 {
-	console.log('tally');
+	// Get all items with time data
+	const timeItems: any[] = [].slice.call(document.querySelectorAll('[data-time]'));
+
+	// Convert data-time on all elements to seconds
+	const seconds: number = timeItems
+		.reduce((total, item) => {
+			const time: number = item.dataset
+				.time.split(':')
+				.map(parseFloat)
+				.reduce((a: number, b: number) => (a * 60) + b);
+
+			return total + time;
+		}, 0);
+
+	let secondsLeft: number = seconds;
+
+	const hours: number = Math.floor(secondsLeft / 3600);
+	secondsLeft = secondsLeft % 3600;
+
+	const mins: number = Math.floor(secondsLeft / 60);
+	secondsLeft = secondsLeft % 60;
+
+	console.log(hours, mins, secondsLeft);
 }
