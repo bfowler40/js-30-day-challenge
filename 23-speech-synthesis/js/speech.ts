ba8f03f91d3837fa31eb898631672e41476b7d5e
Ben Fowler <bfowler40@gmail.com>
2018-01-23 08:00:17 +1100

    configure exercise 23

---
+++ 23-speech-synthesis/js/speech.ts
@@ -0,0 +1,12 @@
+
+class SpeechSynthesis {
+
+	public init(): void {
+		console.log('speechSynthesis');
+	}
+}
+
+const speechSynthesis = new SpeechSynthesis();
+speechSynthesis.init();
+
+export default speechSynthesis;
