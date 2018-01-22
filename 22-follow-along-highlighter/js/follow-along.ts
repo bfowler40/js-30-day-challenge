afaa0642f056ca74443eb6a7f2e03dfa8cd57f66
Ben Fowler <bfowler40@gmail.com>
2018-01-22 19:29:46 +1100

    configure exercise 22

---
+++ 22-follow-along-highlighter/js/follow-along.ts
@@ -0,0 +1,19 @@
+
+class FollowAlong {
+
+	// constructor() {}
+
+	/**
+	 * Init the class
+	 *
+	 * @return {void}
+	 */
+	public init(): void {
+		console.log('hello');
+	}
+}
+
+const followAlong = new FollowAlong();
+followAlong.init();
+
+export default followAlong;
