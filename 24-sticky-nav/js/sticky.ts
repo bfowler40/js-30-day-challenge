47908230830685bfc1321cfa17ca133a1390b72a
Ben Fowler <bfowler40@gmail.com>
2018-01-24 08:41:08 +1100

    solution exercise 24

--- 24-sticky-nav/js/sticky.ts
+++ 24-sticky-nav/js/sticky.ts
@@ -1,11 +1,41 @@
 
 class Sticky {
+	protected _stickyClass: string = 'sticky_active';
+	protected _target: HTMLElement;
+	protected _targetTop: number;
+
+	constructor(target: string) {
+		this._target = (document.querySelector(target) as HTMLElement);
+	}
+
+	/**
+	 * Init the class
+	 *
+	 * @return {void}
+	 */
 	public init() {
-		console.log('init');
+		this._targetTop = this._target.offsetTop;
+
+		window.addEventListener('scroll', this._handleSticky.bind(this));
+	}
+
+	/**
+	 * Manage the sticky element on scroll
+	 *
+	 * @return {void}
+	 */
+	protected _handleSticky() {
+		if (window.scrollY >= this._targetTop) {
+			document.body.style.paddingTop = `${ this._target.offsetHeight}px`;
+			document.body.classList.add(this._stickyClass);
+		} else {
+			document.body.classList.remove(this._stickyClass);
+			document.body.style.paddingTop = `0`;
+		}
 	}
 }
 
-const sticky = new Sticky();
+const sticky = new Sticky('.sticky');
 sticky.init();
 
 export default sticky;
