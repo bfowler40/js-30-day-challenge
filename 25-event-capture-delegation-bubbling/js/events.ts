5230007e0dbb5655efc27d33c5fccf330505ed20
Ben Fowler <bfowler40@gmail.com>
2018-01-25 08:32:31 +1100

    add solution for exercise 25

--- 25-event-capture-delegation-bubbling/js/events.ts
+++ 25-event-capture-delegation-bubbling/js/events.ts
@@ -1,3 +1,29 @@
 {
-	console.log('hello');
+	const divs: HTMLElement[]  = [].slice.call(document.querySelectorAll('div'));
+	const trumpet: HTMLElement = (document.querySelector('.trumpet') as HTMLElement);
+
+	divs.forEach((div: HTMLElement) =>
+		div.addEventListener('click', log));
+
+	trumpet.addEventListener('click', () => {
+		// tslint:disable-next-line
+		console.log('Toot Toot Toot!!');
+	});
+
+	function log(e): void {
+		e.stopPropagation();
+		// tslint:disable-next-line
+		console.log(this.classList.value);
+	}
+}
+
+{
+	// Detect event listener option support
+	let passiveSupported: boolean = false;
+
+	const options = Object.defineProperty({}, 'passive', {
+		get: () => {
+			passiveSupported = true;
+		},
+	});
 }
