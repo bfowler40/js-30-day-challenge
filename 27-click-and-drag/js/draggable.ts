e449a7112b394695235b6bcdee53da47f1ce7412
Ben Fowler <bfowler40@gmail.com>
2018-01-27 08:55:14 +1100

    configure exercise 27

---
+++ 27-click-and-drag/js/draggable.ts
@@ -0,0 +1,18 @@
+
+class Draggable {
+
+	/**
+	 * Init class
+	 *
+	 * @return {void}
+	 */
+	public init(): void {
+		console.log('draggable');
+	}
+
+}
+
+const draggable = new Draggable();
+draggable.init();
+
+export default draggable;
