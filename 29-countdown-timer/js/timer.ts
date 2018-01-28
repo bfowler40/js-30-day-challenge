9e790432c09dc4d46595c7a4371cd130a087a4cb
Ben Fowler <bfowler40@gmail.com>
2018-01-29 07:41:37 +1100

    configure exercise 29

---
+++ 29-countdown-timer/js/timer.ts
@@ -0,0 +1,18 @@
+
+class Timer {
+
+	/**
+	 * Init the class
+	 *
+	 * @return {void}
+	 */
+	public init(): void {
+		console.log('hello');
+	}
+
+}
+
+const timer = new Timer();
+timer.init();
+
+export default timer;
