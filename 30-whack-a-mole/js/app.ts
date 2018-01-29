e3b1984b31fc5af6299d8882659b1f07b9e63050
Ben Fowler <bfowler40@gmail.com>
2018-01-30 07:57:25 +1100

    configure exercise 30

---
+++ 30-whack-a-mole/js/app.ts
@@ -0,0 +1,17 @@
+
+class App {
+	/**
+	 * Init the class
+	 *
+	 * @return {void}
+	 */
+	public init(): void {
+		console.log('init');
+	}
+
+}
+
+const app = new App();
+app.init();
+
+export default app;
---
+++ 30-whack-a-mole/js/index.js
@@ -0,0 +1,5 @@
+// Style
+import '../css/style.scss';
+
+// Scripts
+import './app';
